6c780fb00a26d7f6d68f7e2a7c9f3838bc3cc4a9
dabico <dabico@usi.ch>
2023-02-01 16:30:02 +0100

    Added `ESLint` and enforcing coding style

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+app.get("/", function (req, res) {
+    res.send("Hello World");
+});
 
-app.listen(port)
+app.listen(port);
