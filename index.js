d837ddb16a640f127ce33464315155074da80cec
dabico <dabico@usi.ch>
2023-02-01 17:08:03 +0100

    Added `morgan` request logging package

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const nodeEnv = process.env.NODE_ENV || "development";
+const logger = require("morgan");
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
+
+if (nodeEnv === "development") {
+    app.use(logger("dev"));
+} else {
+    app.use(logger("combined"));
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
