2a02016f8352c7a24f63f298923966e7527b18fe
dabico <dabico@usi.ch>
2023-02-01 16:57:29 +0100

    Added `body-parser` with simple method to demonstrate usage

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
     res.send("Hello World");
+});
+
+app.post("/login", function (req, res) {
+    const name = req.body.name;
+    res.status(200);
+    res.send(`Logged in as: ${name}`);
 });
 
 app.listen(port);
