7a8d24cd6fa3230554622540b5a289bd11d7a2e0
dabico <dabico@usi.ch>
2023-02-02 17:18:47 +0100

    Changed `/label` API endpoint

--- index.js
+++ index.js
@@ -32,19 +32,6 @@ app.use("/", express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/labels", (_, res, next) => {
-    pool.query("SELECT * FROM label")
-        .then(results => { res.status(200).send(results.rows); })
-        .catch(next);
-});
-
-app.post("/labels", (req, res, next) => {
-    const name = req.body.name;
-    pool.query("INSERT INTO label(name) VALUES ($1) RETURNING *", [ name ])
-        .then(results => { res.status(201).send(results.rows[0]); })
-        .catch(next);
-});
-
 app.get("/", (req, res) => {
     res.redirect(req.baseUrl + "/login");
 });
@@ -68,6 +55,16 @@ app.post("/login", async (req, res) => {
 app.get("/:name/review", async (req, res) => {
     const { rows } = await pool.query("SELECT * FROM reviewer WHERE name = $1", [ req.params.name ]);
     res.render("review", { reviewer: rows[0] });
+});
+
+app.post("/label", (req, res) => {
+    pool.query("INSERT INTO label(name) VALUES ($1) RETURNING *", [ req.body.name ])
+        .then(({ rows }) => { res.status(201).send(rows[0]); })
+        .catch(({ code }) => {
+            if (code !== "23505") res.status(500);
+            else res.status(409);
+            res.end();
+        });
 });
 
 app.get("/error", (_, res) => {
