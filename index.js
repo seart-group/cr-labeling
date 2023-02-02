304c83bb7edd584d1c018f3dbe04f0dc0cfc63dd
dabico <dabico@usi.ch>
2023-02-02 16:26:25 +0100

    Removed API endpoints that I do not intend to use

--- index.js
+++ index.js
@@ -32,19 +32,6 @@ app.use("/", express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/reviewers", (_, res, next) => {
-    pool.query("SELECT * FROM reviewer")
-        .then(results => { res.status(200).send(results.rows); })
-        .catch(next);
-});
-
-app.post("/reviewers", (req, res, next) => {
-    const name = req.body.name;
-    pool.query("INSERT INTO reviewer(name) VALUES ($1) RETURNING *", [ name ])
-        .then(results => { res.status(201).send(results.rows[0]); })
-        .catch(next);
-});
-
 app.get("/labels", (_, res, next) => {
     pool.query("SELECT * FROM label")
         .then(results => { res.status(200).send(results.rows); })
