ff5efe995f2944da93472c0a0ba8903dd543db75
dabico <dabico@usi.ch>
2023-02-09 14:02:22 +0100

    Instances now properly loaded from CSV using `\COPY`

--- deployment/data/init-data.sh
+++ deployment/data/init-data.sh
@@ -7,3 +7,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" \
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" \
   -c "\COPY reviewer(name) FROM '/docker-entrypoint-initdb.d/reviewer.txt';"
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" \
+  -c "\COPY instance(task, work, category, input_code, input_nl, output, target) FROM '/docker-entrypoint-initdb.d/instance.csv' HEADER DELIMITER ',' QUOTE '\"' CSV;"
