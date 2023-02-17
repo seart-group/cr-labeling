e6e63b3644644ee705b285d9a434a3d840d577f3
dabico <dabico@usi.ch>
2023-02-17 16:03:35 +0100

    Renamed `target` to `params`

--- index.js
+++ index.js
@@ -123,16 +123,16 @@ app.post("/label", (req, res) => {
 });
 
 app.get("/conflicts/:id", async (req, res) => {
-    let target = [ req.params.id ];
-    const { rows: [ instance ] } = await pool.query("SELECT * FROM instance_review_conflict WHERE id = $1", target);
+    let params = [ req.params.id ];
+    const { rows: [ instance ] } = await pool.query("SELECT * FROM instance_review_conflict WHERE id = $1", params);
     if (!instance) {
         res.render("error", {
             icon: "bi-none",
             title: "Conflict not found or already resolved!"
         });
     }
-    const { rows: reviews } = await pool.query("SELECT * FROM instance_review_details($1)", target);
-    const { rows: discards } = await pool.query("SELECT * FROM instance_discard_details($1)", target);
+    const { rows: reviews } = await pool.query("SELECT * FROM instance_review_details($1)", params);
+    const { rows: discards } = await pool.query("SELECT * FROM instance_discard_details($1)", params);
     res.render("conflict", {
         instance: instance,
         reviews: reviews,
