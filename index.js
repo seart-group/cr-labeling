0adbb2ac0c422508e4b4038628db5947b6c845c3
dabico <dabico@usi.ch>
2023-02-27 11:56:13 +0100

    Added a paginated view for `instances`, similar to `conflicts`

--- index.js
+++ index.js
@@ -130,6 +130,27 @@ app.post("/label", (req, res) => {
         });
 });
 
+app.get("/instances", async (req, res) => {
+    const current = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const id = req.query.id || 1;
+    const order = (id > 0) ? "ASC" : "DESC";
+    const query = `SELECT id FROM instance ORDER BY id ${order} OFFSET $1 LIMIT $2`;
+    const { rows: instances } = await pool.query(query, [ (current - 1) * limit, limit ]);
+    const { rows: [ { count: items } ] } = await pool.query("SELECT COUNT(id) FROM instance");
+    const pages = Math.ceil(items / req.query.limit);
+    res.render("instances", {
+        instances: instances,
+        pagination: {
+            items: items,
+            pages: pages,
+            current: current,
+            limit: limit,
+            id: id
+        }
+    });
+});
+
 app.get("/instances/:id", async (req, res) => {
     const params = [ req.params.id ];
     const { rows: [ instance ] } = await pool.query("SELECT * FROM instance WHERE id = $1", params);
@@ -147,12 +168,7 @@ app.get("/conflicts", async (req, res) => {
     const limit = req.query.limit || 10;
     const id = req.query.id || 1;
     const order = (id > 0) ? "ASC" : "DESC";
-    const query =
-        `SELECT id
-         FROM instance_review_conflict
-         ORDER BY id ${order}
-         OFFSET $1
-         LIMIT $2`;
+    const query = `SELECT id FROM instance_review_conflict ORDER BY id ${order} OFFSET $1 LIMIT $2`;
     const { rows: conflicts } = await pool.query(query, [ (current - 1) * limit, limit ]);
     const { rows: [ { count: items } ] } = await pool.query(
         "SELECT COUNT(id) FROM instance_review_conflict"
